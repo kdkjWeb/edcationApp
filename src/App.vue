354d1eea9017e1f67e75cb77f60ee397d668fedf
sherlly <511987975@qq.com>
2018-03-02 12:10:09 +0800

    no message

--- src/App.vue
+++ src/App.vue
@@ -11,5 +11,29 @@ export default {
 </script>
 
 <style>
-
+  html,body{
+    width:100%;
+    height:100%;
+  }
+  *{
+    padding:0;
+    margin:0;
+  }
+  a {
+    text-decoration: none;
+  }
+  li {
+    list-style: none;
+  }
+  .clear:after {
+    content:'';
+    display:block;
+    clear: both;
+  }
+  img {
+    border:none;
+  }
+  #app {
+    height:100%;
+  }
 </style>
