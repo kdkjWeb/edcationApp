403b3ac279b84756090f1205d39d568a687d9117
sherlly <511987975@qq.com>
2018-03-02 11:24:45 +0800

    修改无法运行问题

--- src/page/index/index.vue
+++ src/page/index/index.vue
@@ -1,9 +1,15 @@
 <template>
   <div>
-    
+
   </div>
 </template>
 <script>
     export default require('./indexCtr.js');
 </script>
+<style scoped>
+  html,body{
+    width:100%;
+    height:100%;
+  }
+</style>
 
