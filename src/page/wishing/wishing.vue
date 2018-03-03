49568af76e6e4f4e766a40fc5f9e1fcb26827d0c
Mac <1270201010@qq.com>
2018-03-03 12:28:25 +0800

    许愿树页面

--- src/page/wishing/wishing.vue
+++ src/page/wishing/wishing.vue
@@ -1,15 +1,102 @@
 <template>
-  <div>
-6666
+  <div id="wishing">
+    <!-- 许愿树 -->
+    <div class="wishing">
+      <span class="leaf" v-for="(item,index) in arrWishing" :key="index"></span>
+    </div>
+
+    <!-- 许愿人及许愿内容 -->
+    <div class="wishing_info">
+        <p class="wishing_info_text">许愿人</p>
+        <input class="wishing_info_input" type="text" v-model="wishing.wishingTxt">
+        <p class="wishing_info_text">愿里心语</p>
+        <!-- <input class="wishing_info_input" type="text"> -->
+        <textarea class="wishing_info_textarea" name="" v-model="wishing.wishingCon"></textarea>
+    </div>
+    <!-- 操作按钮 -->
+    <div class="btn">
+        <div @click="send">
+          <span class="iconfont icon-fasong"></span>发送</div>
+        <div @click="next">下一棵树</div>
+    </div>
   </div>
 </template>
 <script>
     export default require('./wishingCtr.js');
 </script>
 <style scoped>
-  html,body{
-    width:100%;
-    height:100%;
+  #wishing{
+    background: #e9e9e9;
+    position: relative;
+  }
+  .wishing{
+    width: 100%;
+    padding-bottom: 144%;
+    height: 0;
+    background: url('../../assets/wishing.png');
+    background-size: contain;
+  }
+  .wishing_info{
+    width: 90%;
+    margin: 0 auto;
+  }
+  .wishing_info_text{
+    color: #010101;
+    font-size: 14px;
+    padding: 10px 0;
+  }
+  .wishing_info_input{
+    width: 100%;
+    height: 30px;
+    border-radius: 20px;
+    border: none;
+    outline: none;
+    padding-left: 8px;
+  }
+  .wishing_info_textarea{
+    width: calc(100% - 8px);
+    height: 80px;
+    border-radius: 10px;
+    border: none;
+    outline: none;
+    padding-left: 8px;
+    padding-right: 8px;
+  }
+  .btn{
+    width: 90%;
+    height: 35px;
+    line-height: 35px;
+    margin: 0 auto;
+    display: flex;
+    display: -webkit-flex;
+    justify-content: space-between;
+    margin-top: 15px;
+  }
+  .btn div{
+    width: 47%;
+    height: 100%;
+    background: #ffb93e;
+    text-align: center;
+    border-radius: 20px;
+    color: #fff;
+    font-size: 14px;
+  }
+  .btn div span.icon-fasong{
+    position: relative;
+    top: -2px;
+    font-size: 22px;
+    vertical-align: middle;
+    padding-right: 5px;
+  }
+
+
+  .leaf{
+    position: absolute;
+    width: 30px;
+    height: 50px;
+    display: inline-block;
+    background-image: url('../../assets/wishPkg.png');
+    background-size: cover;
   }
 </style>
 
--- src/page/wishing/wishingCtr.js
+++ src/page/wishing/wishingCtr.js
@@ -4,13 +4,78 @@
 export default {
   data(){
     return {
-      
+      wishing:{
+        wishingTxt: '',
+        wishingCon: ''
+      },
+      arrWishing: ['a','b','c','a','b','c','a','b','c','a','b','c','a','b','c','a','b','c','a','b','c','a','b','c','a','b','c','a','b','c'],
+      arrPositon: [],
+      ret: []
     }
   },
   methods:{
-    
+      //点击发送按钮
+      send(){
+        
+        if(!this.wishing.wishingTxt||!this.wishing.wishingCon){
+            console.log('信息填入不全')
+            return;
+        }
+        console.log(this.wishing.wishingTxt,this.wishing.wishingCon)
+      },
+      //点击下一棵按钮
+      next(){
+        console.log('下一棵树')
+      },
+      //随机生成位置
+      random(){
+        let leaf = document.getElementsByClassName('leaf');
+        let Width = document.body.clientWidth-30;
+        let Height = Width;
+        for(let i=0; i<leaf.length; i++){
+             let left = Math.random()*Width;
+             let top = this.getRandom(65,Height);
+             this.arrPositon.push({
+               left: left,
+               top: top
+             });
+             
+              this.getPosition();
+              console.log(this.ret)
+              for(let i=0; i<this.ret.length; i++){
+                leaf[i].style.top = this.ret[i].top + 'px'
+                leaf[i].style.left = this.ret[i].left + 'px'
+              }
+             //console.log(this.arrPositon)
+             //leaf[i].style.top = top + 'px'
+             //leaf[i].style.left = left + 'px'
+        }
+      },
+      //生成指定范围内的不重复随机数
+      
+      //生成指定范围内的随机数
+      getRandom(start, end) {
+          let length = end - start;
+          let num = Math.random() * (length) + start;
+          return num;
+        },
+      //遍历随机位置数组，防止位置重叠
+       getPosition(){
+        // var ret = [];
+        for (var i = 0, j = this.arrPositon.length; i < j; i++) {
+            if (this.ret.indexOf(this.arrPositon[i]) === -1) {
+                this.ret.push(this.arrPositon[i]);
+            }else{
+              this.getRandom()
+            }
+        }
+         return this.ret;
+        }
     },
     created(){
         
+    },
+    mounted(){
+      this.random()
     }
 }
