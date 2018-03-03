a16508a5bbb9561cb761d44973ea26ff87c7be75
sherlly <511987975@qq.com>
2018-03-03 14:49:51 +0800

    no message

--- src/page/tabBar/tabBarCtr.js
+++ src/page/tabBar/tabBarCtr.js
@@ -2,35 +2,41 @@
  * Created by kdkjPC on 2018/3/2.
  */
 export default {
-    data(){
-        return {
-            active:1
-        }
-    },
-    methods:{
-        toRouter(index){
-            switch(index){
-                case 1:
-                    this.active = index;
-                    this.$router.push({
-                        path:'/tabBar'
-                    });
-                    break;
-                case 2:
-                    this.active = index;
-                    this.$router.push({
-                        path:'/wishing'
-                    });
-                    break;
-                default:
-                    this.active = index;
-                    this.$mint.Toast({
-                        message: '功能正在开发，敬请期待！',
-                        position: 'center',
-                        duration: 500
-                    });
-                    break;
-            }
-        }
+  data(){
+    return {
+      active: 1
     }
+  },
+  methods: {
+    toRouter(index){
+      this.active = index;
+      switch (index) {
+        case 1:
+          this.$router.push({
+            path: '/tabBar'
+          });
+          break;
+        case 2:
+          this.$router.push({
+            path: '/wishing'
+          });
+          break;
+        case 3:
+          window.location.reload();
+          break;
+        case 5:
+          this.$router.push({
+            path: '/'
+          });
+          break;
+        default:
+          this.$mint.Toast({
+            message: '功能正在开发，敬请期待！',
+            position: 'center',
+            duration: 500
+          });
+          break;
+      }
+    }
+  }
 }
